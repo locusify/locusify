b9e959a8da3d54523339da78b6f5251ead269b39
daiqin <daiqin1046@gmail.com>
2025-10-16 15:36:30 +0800

    feat(layout): 添加路由保护组件包裹布局
    在布局组件中引入 ProtectedRoute 组件，对整个布局进行路由保护，
    确保只有授权用户才能访问页面内容。

--- apps/web/src/layout/index.tsx
+++ apps/web/src/layout/index.tsx
@@ -3,17 +3,20 @@ import { Suspense } from 'react'
 import { Outlet } from 'react-router'
 import { Spinner } from '@/components/ui/spinner'
 import Navbar from './components/navbar'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const Layout: FC = () => {
   return (
-    <div className="h-screen flex flex-col">
-      <main className="flex-1 overflow-auto shrink-0">
-        <Suspense fallback={<Spinner />}>
-          <Outlet />
-        </Suspense>
-      </main>
-      <Navbar />
-    </div>
+    <ProtectedRoute>
+      <div className="h-screen flex flex-col">
+        <main className="flex-1 overflow-auto shrink-0">
+          <Suspense fallback={<Spinner />}>
+            <Outlet />
+          </Suspense>
+        </main>
+        <Navbar />
+      </div>
+    </ProtectedRoute>
   )
 }
 
